d783567d77e0a3dc89b686980285caa9d5172dcc
amitchahar9837 <amitchahar9837@gmail.com>
2024-07-31 15:43:35 +0530

    some error are handleded

--- client/src/pages/Categories.jsx
+++ client/src/pages/Categories.jsx
@@ -12,7 +12,7 @@ const Categories = () => {
         <div className='grid grid-cols-3 px-48 py-16 gap-12'>
         {categories.map((c) => (
             <div className="" key={c._id}>
-                <Link to={`/category/${c.slug}`}
+                <Link to={`/categories/${c.slug}`}
                 >
               <div className="px-3 py-5 text-xl text-center border-2 border-gray-500 font-medium hover:bg-gray-400">
               {c.name}
